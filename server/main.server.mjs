22e72e67414dff7a20bc95b18dbce1fd5e01068c
Haroon Khan <haroon.khan.am@gmail.com>
2025-10-06 21:11:36 +0500

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-4GZH5F3V.mjs";import"./chunk-OJ7TVAO5.mjs";import"./chunk-SWN4TK22.mjs";import"./chunk-2BWWHEYA.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
